68163f6b5ac43be11fbe44f433e29c44635641d2
Amanda Cortez <amanda_cortez_2012@hotmail.com>
2025-03-10 08:31:13 -0300

    Projetos

--- js/script.js
+++ js/script.js
@@ -74,3 +74,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+// Filtros
+function filtrarProjetos(categoria) {
+    let projetos = document.querySelectorAll('.projeto');
+
+    projetos.forEach(projeto => {
+        if (categoria === 'todos' || projeto.dataset.categoria === categoria) {
+            projeto.style.display = 'block'; // Mostra o projeto
+        } else {
+            projeto.style.display = 'none'; // Esconde o projeto
+        }
+    });
+}
